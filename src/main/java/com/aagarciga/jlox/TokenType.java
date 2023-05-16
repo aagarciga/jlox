05da2f44cc971543c28868e0b9e5b68493491735
Alex Alvarez Gárciga <aagarciga@gmail.com>
2023-05-15 21:05:30 -0400

    Initial Commit. Begining of interpreter implementation

---
+++ src/main/java/com/aagarciga/jlox/TokenType.java
@@ -0,0 +1,26 @@
+package com.aagarciga.jlox;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+/**
+ *
+ * @author alexalvarezgarciga
+ */
+enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+    EOF
+}
