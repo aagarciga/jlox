9b5b76790b3e7943a7254e8bc5b1077daecc743b
Alex Alvarez Gárciga <aagarciga@gmail.com>
2023-05-16 00:39:21 -0400

    Implementing some Visitor overrides for the Interpreter

--- src/main/java/com/aagarciga/jlox/Interpreter.java
+++ src/main/java/com/aagarciga/jlox/Interpreter.java
@@ -4,10 +4,88 @@
  */
 package com.aagarciga.jlox;
 
+import static com.aagarciga.jlox.TokenType.MINUS;
+
 /**
  *
  * @author alexalvarezgarciga
  */
-public class Interpreter {
-    
+public class Interpreter implements Expression.Visitor<Object> {
+
+    @Override
+    public Object visitLiteralExpression(Expression.Literal expression) {
+        return expression.value;
+    }
+
+    @Override
+    public Object visitUnaryExpression(Expression.Unary expression) {
+        Object right = evaluate(expression.right);
+
+        switch (expression.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                return -(double) right;
+        }
+
+        return null;
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) {
+            return false;
+        }
+        if (object instanceof Boolean) {
+            return (boolean) object;
+        }
+        return true;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) {
+            return true;
+        }
+        if (a == null) {
+            return false;
+        }
+
+        return a.equals(b);
+    }
+
+    @Override
+    public Object visitGroupingExpression(Expression.Grouping expression) {
+        return evaluate(expression.expression);
+    }
+
+    private Object evaluate(Expression expression) {
+        return expression.accept(this);
+    }
+
+    @Override
+    public Object visitBinaryExpression(Expression.Binary expression) {
+        Object left = evaluate(expression.left);
+        Object right = evaluate(expression.right);
+
+        switch (expression.operator.type) {
+            case MINUS:
+                return (double) left - (double) right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+                if (left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+            case SLASH:
+                return (double) left / (double) right;
+            case STAR:
+                return (double) left * (double) right;
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+        }
+
+        return null;
+    }
 }
--- src/main/java/com/aagarciga/jlox/Parser.java
+++ src/main/java/com/aagarciga/jlox/Parser.java
@@ -12,162 +12,174 @@ import java.util.List;
  * @author alexalvarezgarciga
  */
 public class Parser {
-    private static class ParseError extends RuntimeException {}
-    
+
+    private static class ParseError extends RuntimeException {
+    }
+
     private final List<Token> tokens;
     private int current = 0;
-    
+
     Parser(List<Token> tokens) {
         this.tokens = tokens;
     }
-    
-    Expression parse(){
-        try{
+
+    Expression parse() {
+        try {
             return expression();
         } catch (ParseError error) {
             return null;
         }
     }
-    
-    private Expression expression(){
+
+    private Expression expression() {
         return equality();
     }
-    
+
     // equality       → comparison ( ( "!=" | "==" ) comparison )* ;
-    private Expression equality(){
+    private Expression equality() {
         Expression expr = comparison();
-        
-        while(match(BANG_EQUAL, EQUAL_EQUAL)) {
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
             Token operator = previous();
             Expression right = comparison();
             expr = new Expression.Binary(expr, operator, right);
         }
-        
+
         return expr;
     }
-    
+
     //comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
-    private Expression comparison(){
+    private Expression comparison() {
         Expression expr = term();
-        
-        while(match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)){
+
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
             Token operator = previous();
             Expression right = term();
             expr = new Expression.Binary(expr, operator, right);
         }
-        
+
         return expr;
     }
-    
-    private Expression term(){
+
+    private Expression term() {
         Expression expr = factor();
-        
-        while(match(MINUS, PLUS)){
+
+        while (match(MINUS, PLUS)) {
             Token operator = previous();
             Expression right = factor();
             expr = new Expression.Binary(expr, operator, right);
         }
-        
+
         return expr;
     }
-    
-    private Expression factor(){
+
+    private Expression factor() {
         Expression expr = unary();
-        
-        while(match(SLASH, STAR)){
+
+        while (match(SLASH, STAR)) {
             Token operator = previous();
             Expression right = unary();
             expr = new Expression.Binary(expr, operator, right);
         }
-        
+
         return expr;
     }
-    
+
     // unary          → ( "!" | "-" ) unary
     //                  | primary ;
-    
-    private Expression unary(){
-        if(match(BANG, MINUS)){
+    private Expression unary() {
+        if (match(BANG, MINUS)) {
             Token operator = previous();
             Expression right = unary();
             return new Expression.Unary(operator, right);
         }
-        
+
         return primary();
     }
-    
+
     // primary        → NUMBER | STRING | "true" | "false" | "nil"
     //                  | "(" expression ")" ;
-    private Expression primary(){
-        if(match(FALSE)) return new Expression.Literal(false);
-        if(match(TRUE)) return new Expression.Literal(true);
-        if(match(NIL)) return new Expression.Literal(null);
-        
-        if(match(NUMBER, STRING)) {
+    private Expression primary() {
+        if (match(FALSE)) {
+            return new Expression.Literal(false);
+        }
+        if (match(TRUE)) {
+            return new Expression.Literal(true);
+        }
+        if (match(NIL)) {
+            return new Expression.Literal(null);
+        }
+
+        if (match(NUMBER, STRING)) {
             return new Expression.Literal(previous().literal);
         }
-        
-        if(match(LEFT_PAREN)){
+
+        if (match(LEFT_PAREN)) {
             Expression expr = expression();
             consume(RIGHT_PAREN, "Expect ')' after expression.");
             return new Expression.Grouping(expr);
         }
-        
+
         throw error(peek(), "Expect expression.");
     }
 
     private boolean match(TokenType... types) {
-        for(TokenType type: types){
-            if(check(type)){
+        for (TokenType type : types) {
+            if (check(type)) {
                 advance();
                 return true;
             }
         }
         return false;
     }
-    
-    private Token consume(TokenType type, String message){
-        if(check(type)) return advance();
-        
+
+    private Token consume(TokenType type, String message) {
+        if (check(type)) {
+            return advance();
+        }
+
         throw error(peek(), message);
     }
-    
+
     private boolean check(TokenType type) {
-        if(isAtEnd())
+        if (isAtEnd()) {
             return false;
+        }
         return peek().type == type;
     }
-    
+
     private Token advance() {
-        if(!isAtEnd())
+        if (!isAtEnd()) {
             current++;
+        }
         return previous();
     }
-    
-    private boolean isAtEnd(){
+
+    private boolean isAtEnd() {
         return peek().type == EOF;
     }
-    
+
     private Token peek() {
         return tokens.get(current);
     }
-    
-    private Token previous(){
+
+    private Token previous() {
         return tokens.get(current - 1);
-    }    
-    
-    private ParseError error(Token token, String message){
+    }
+
+    private ParseError error(Token token, String message) {
         Jlox.error(token, message);
         return new ParseError();
     }
-    
-    private void synchronize(){
+
+    private void synchronize() {
         advance();
-        
-        while(!isAtEnd()) {
-            if(previous().type == SEMICOLON) 
+
+        while (!isAtEnd()) {
+            if (previous().type == SEMICOLON) {
                 return;
-            
+            }
+
             switch (peek().type) {
                 case CLASS:
                 case FUN:
@@ -179,12 +191,10 @@ public class Parser {
                 case RETURN:
                     return;
             }
-            
+
             advance();
         }
-        
+
     }
-    
+
 }
-
-
